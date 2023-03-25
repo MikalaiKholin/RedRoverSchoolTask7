90c63e8ecb2a359970fccbb2a39086885ced1376
MikalaiKholin <48877386+MikalaiKholin@users.noreply.github.com>
2023-03-25 20:45:30 +0300

    refactor : task 7.6 refactor

--- src/KataFighter.java
+++ src/KataFighter.java
@@ -7,16 +7,15 @@ public class KataFighter {
             secondFighter = fighter1;
         }
         while (true){
-            secondFighter.health -= firstFighter.damagePerAttack;
-            if (secondFighter.health <= 0) {
+            if ((secondFighter.health -= firstFighter.damagePerAttack) <= 0) {
                 System.out.printf("%s attacks %s: %s now has %d health and is dead. %s wins.%n",
                         firstFighter.name, secondFighter.name, secondFighter.name, secondFighter.health, firstFighter.name);
                 return firstFighter.name;
             }
             System.out.printf("%s attacks %s; %s now has %d health.%n",
                     firstFighter.name, secondFighter.name, secondFighter.name, secondFighter.health);
-            firstFighter.health -=secondFighter.damagePerAttack;
-            if (firstFighter.health <= 0) {
+
+            if ((firstFighter.health -= secondFighter.damagePerAttack) <= 0) {
                 System.out.printf("%s attacks %s: %s now has %d health and is dead. %s wins.%n",
                         secondFighter.name, firstFighter.name, firstFighter.name, firstFighter.health, secondFighter.name);
                 return secondFighter.name;
